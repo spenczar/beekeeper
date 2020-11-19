3b149411a3b697b5aca4d2f8a65a2d38726ce133
Spencer Nelson <swnelson@uw.edu>
2020-11-19 12:12:58 -0800

    Add representation of a puzzle

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fs::File;
+use std::fs::{File, read_to_string};
 use std::io;
 use std::io::BufRead;
 use std::vec::Vec;
@@ -17,7 +17,48 @@ fn load_dictionary() -> io::Result<Vec<String>> {
     Ok(lines)
 }
 
+/// Read a single puzzle from a file.
+///
+/// The file should have 7 characters. The first character is the center of the
+/// Spelling Bee puzzle.
+fn load_puzzle_from_file(path: &str) -> io::Result<Puzzle> {
+    let raw = read_to_string(path)?;
+    let count = raw.chars().count();
+    assert_eq!(8, count);
+
+    let chars: Vec<char> = raw.chars().collect();
+    let p = Puzzle {
+        center_letter: chars[0],
+        outer_letters: [
+            chars[1],
+            chars[2],
+            chars[3],
+            chars[4],
+            chars[5],
+            chars[6],
+        ],
+    };
+    Ok(p)
+}
+
+struct Puzzle {
+    center_letter: char,
+    outer_letters: [char; 6],
+}
+
+impl Puzzle {
+    fn to_string(&self) -> String {
+        let mut result = String::from(self.center_letter);
+        result.push_str(": ");
+        result.extend(self.outer_letters.iter());
+        result
+    }
+}
+
 fn main() {
     let dictionary = load_dictionary().unwrap();
     println!("There are {} words in the dictionary.", dictionary.len());
+
+    let puzzle = load_puzzle_from_file("puzzle.txt").unwrap();
+    println!("Puzzle: {}", puzzle.to_string());
 }
