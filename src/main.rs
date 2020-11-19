4b707a45cf0e7fa099c050e6156a7985266c95ad
Spencer Nelson <swnelson@uw.edu>
2020-11-19 11:48:30 -0800

    Load words from unix dictionary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::fs::File;
+use std::io;
+use std::io::BufRead;
+use std::vec::Vec;
+
+const WORDS_FILE_PATH: &str = "/usr/share/dict/words";
+
+/// Load all the words from the unix dictionary.
+fn load_dictionary() -> io::Result<Vec<String>> {
+    let file = File::open(WORDS_FILE_PATH)?;
+    let reader = io::BufReader::new(file);
+
+    let mut lines = Vec::new();
+    for line in reader.lines() {
+        lines.push(line?);
+    }
+    Ok(lines)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let dictionary = load_dictionary().unwrap();
+    println!("There are {} words in the dictionary.", dictionary.len());
 }
